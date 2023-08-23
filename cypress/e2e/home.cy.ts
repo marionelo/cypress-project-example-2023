83b2d24f733e63d9aa9924d87f34293195072309
Mario Sánchez <m.sanchez@cyberpuerta.mx>
2023-08-23 06:59:19 -0600

    mejoras

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,34 +4,46 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct text", () => {
-    cy.get("[data-test='hero-heading']").contains(
-      "Testing Next.js Applications with Cypress"
-    )
-  })
+  context("Hero section", () => {
+    it("the h1 contains the correct text", () => {
+      cy.get("[data-test='hero-heading']").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
-  })
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
+  });
+  
 
-  it("the h1 contains the correct text", () => {
-    cy.getByData("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
-  })
+  context("Hero section 2", () => {
+    it("the h1 contains the correct text", () => {
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
 
-  it.only("allows users to subscribe to the email list", () => {
-    cy.getByData("email-input").type("tom@aol.com")
-    cy.getByData("submit-button").click()
-    cy.getByData("success-message").should("exist").contains("tom@aol.com")
-  })
+    it("allows users to subscribe to the email list", () => {
+      cy.getByData("email-input").type("tom@aol.com")
+      cy.getByData("submit-button").click()
+      cy.getByData("success-message").should("exist").contains("tom@aol.com")
+    })
 
-  it.only("does NOT allow an invalid email address", () => {
-    cy.getByData("email-input").type("tom")
-    cy.getByData("submit-button").click()
-    cy.getByData("success-message").should("not.exist")
+    it("does NOT allow an invalid email address", () => {
+      cy.getByData("email-input").type("tom")
+      cy.getByData("submit-button").click()
+      cy.getByData("success-message").should("not.exist")
+    })
+  });
+
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
   })
   
 })
